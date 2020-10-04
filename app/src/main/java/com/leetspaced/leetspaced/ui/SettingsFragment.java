7849b561961f58b8ce712b609c5e6ee535a41d9e
Avin Sharma <sharmaavin@hotmail.com>
2020-10-03 21:36:22 -0400

    Add Validation to duration Edittext Preference

--- app/src/main/java/com/leetspaced/leetspaced/ui/SettingsFragment.java
+++ app/src/main/java/com/leetspaced/leetspaced/ui/SettingsFragment.java
@@ -2,9 +2,13 @@ package com.leetspaced.leetspaced.ui;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.EditTextPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -14,10 +18,34 @@ import androidx.preference.PreferenceScreen;
 import com.leetspaced.leetspaced.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+    public static final String TAG = SettingsFragment.class.getSimpleName();
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings_pref);
         updateListPreferenceSummary();
+        addValidationDurationEditTextPref();
+    }
+
+    private void addValidationDurationEditTextPref() {
+        EditTextPreference bucketOnePref = (EditTextPreference) getPreferenceScreen().findPreference(getString(R.string.bucket_one_duration_key));
+        EditTextPreference bucketTwoPref = (EditTextPreference) getPreferenceScreen().findPreference(getString(R.string.bucket_two_duration_key));
+
+        Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                try {
+                    int duration = Integer.parseInt((String)newValue);
+                    if (duration > 0 && duration < 91) return true;
+                }
+                catch (NumberFormatException nfe){
+                    Log.d(TAG, nfe.toString());
+                }
+                Toast.makeText(getContext(), "Enter a number between 1 and 90", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        };
+        bucketOnePref.setOnPreferenceChangeListener(listener);
+        bucketTwoPref.setOnPreferenceChangeListener(listener);
     }
 
     private void updateListPreferenceSummary() {
